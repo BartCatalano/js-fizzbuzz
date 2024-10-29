5511aa8f6ae0fa2114d7d68832a414ad55923f34
BartCatalano <catalanobartolomeo@gmail.com>
2024-10-29 15:46:18 +0100

    feat: eserzcizio finito

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-alert("ciao")
+// Creazione generatore numeri base
+let somma = 0;   //    variabile d'appoggio
+
+for (let i = 1; i <= 100; i++) {
+    // crezione condizionale per multipli di 3 e 5
+if (i % 3 === 0 && i % 5 === 0 ) {
+    console.log("FizzBuzz");
+}
+// crezione condizionale per multipli di 3
+else if (i % 3 === 0){
+    console.log("Fizz");
+}
+// crezione condizionale per multipli di 5
+else if (i % 5 === 0) {
+    console.log("Buzz");
+}
+// Condizionale senza multipli
+else {
+console.log(i);
+}
+somma=somma+1;
+
+
+
+}
+    
